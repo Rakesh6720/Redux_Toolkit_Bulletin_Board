2625ea24bd796c1cac8b4f0b79577430f6b06450
Rakesh Baruah <rbaruah@gmail.com>
2023-03-27 16:35:53 -0500

    add time component

--- src/features/post/PostsLists.js
+++ src/features/post/PostsLists.js
@@ -1,6 +1,8 @@
 import { useSelector } from "react-redux";
 import React from "react";
 import { selectAllPosts } from "./postsSlice";
+import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
 
 const PostsLists = () => {
   const posts = useSelector(selectAllPosts);
@@ -9,6 +11,10 @@ const PostsLists = () => {
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+      </p>
     </article>
   ));
 
