306635fc1ddd1bdfe4a06f48c09e49178edca6b3
Rakesh Baruah <rbaruah@gmail.com>
2023-03-27 16:09:57 -0500

    add posts list and add post form components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import PostsLists from "./features/post/PostsLists";
+import AddPostForm from "./features/post/AddPostForm";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <AddPostForm />
+      <PostsLists />
+    </main>
   );
 }
 
